2665fb9380b00893b836a24d639c3012702e8420
Trevor Siemens <trevsiemens@gmail.com>
2013-05-20 14:43:47 -0700

    Added filter onto skills

--- pathfinder-toolkit/src/com/lateensoft/pathfinder/toolkit/stats/PTSkillSet.java
+++ pathfinder-toolkit/src/com/lateensoft/pathfinder/toolkit/stats/PTSkillSet.java
@@ -1,5 +1,7 @@
 package com.lateensoft.pathfinder.toolkit.stats;
 
+import java.util.ArrayList;
+
 import android.content.Context;
 import android.content.res.Resources;
 
@@ -28,8 +30,36 @@ public class PTSkillSet {
 			return null;
 	}
 	
+	/**
+	 * @return reference to skill at index. Index corresponds to array created by getTrainedSkills
+	 */
+	public PTSkill getTrainedSkill(int index){
+		int trainedSkillIndex = 0;
+		for (int i = 0; i < mSkills.length; i++) {
+			if (mSkills[i].getRank() > 0) {
+				if (trainedSkillIndex == index){
+					return mSkills[i];
+				} else {
+					trainedSkillIndex++;
+				}
+			}
+		}
+		return null;
+	}
+	
 	public PTSkill[] getSkills(){
 		return mSkills;
+	}
+	
+	public PTSkill[] getTrainedSkills(){
+		ArrayList<PTSkill> trainedSkills = new ArrayList<PTSkill>();
+		for (int i = 0; i < mSkills.length; i++) {
+			if (mSkills[i].getRank() > 0) {
+				trainedSkills.add(mSkills[i]);
+			}
+		}
+		PTSkill[] returnArray = new PTSkill[trainedSkills.size()];
+		return trainedSkills.toArray(returnArray);
 	}
 	
 	//can set/get through reff to skill
