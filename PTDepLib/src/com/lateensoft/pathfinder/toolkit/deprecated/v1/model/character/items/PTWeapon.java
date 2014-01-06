9365b15477d931b840c64c3e40beb193f633311a
Trevor Siemens <trevsiemens@gmail.com>
2014-01-05 17:52:26 -0800

    added the 1.1.1 deprecated storage library so we can get out of using gson

---
+++ PTDepLib/src/com/lateensoft/pathfinder/toolkit/deprecated/v1/model/character/items/PTWeapon.java
@@ -0,0 +1,167 @@
+package com.lateensoft.pathfinder.toolkit.deprecated.v1.model.character.items;
+
+import com.lateensoft.pathfinder.toolkit.deprecated.R;
+
+import android.content.Context;
+import android.content.res.Resources;
+
+public class PTWeapon extends PTItem {
+	int mTotalAttackBonus;
+	String mDamage;
+	String mCritical;
+	int mRange;
+	String mSpecialProperties;
+	int mAmmunition;
+	String mType;
+	String mSize;
+	
+	public PTWeapon() {
+		super();
+		mTotalAttackBonus = 0;
+		mDamage = new String("");
+		mCritical = new String("x2");
+		mRange = 5;
+		mSpecialProperties = new String("");
+		mAmmunition = 0;
+		mType = new String("");
+		mSize = "M";
+	}
+	
+	public PTWeapon(String name) {
+		this();
+		mName = name;
+	}
+
+	/**
+	 * @return the mTotalAttackBonus
+	 */
+	public int getTotalAttackBonus() {
+		return mTotalAttackBonus;
+	}
+
+	/**
+	 * @param mTotalAttackBonus the mTotalAttackBonus to set
+	 */
+	public void setTotalAttackBonus(int mTotalAttackBonus) {
+		this.mTotalAttackBonus = mTotalAttackBonus;
+	}
+	
+	public int getSizeInt() {
+		String[] sizeArray = {"S", "M", "L"};
+		for(int i = 0; i < sizeArray.length; i++) {
+			if(mSize.equals(sizeArray[i]))
+				return i;
+		}
+		return 1;
+	}
+	
+	/**
+	 * @return the mDamage
+	 */
+	public String getDamage() {
+		return mDamage;
+	}
+
+	/**
+	 * @param mDamage the mDamage to set
+	 */
+	public void setDamage(String mDamage) {
+		this.mDamage = mDamage;
+	}
+
+	/**
+	 * @return the mCritical
+	 */
+	public String getCritical() {
+		return mCritical;
+	}
+
+	/**
+	 * @param mCritical the mCritical to set
+	 */
+	public void setCritical(String mCritical) {
+		this.mCritical = mCritical;
+	}
+
+	/**
+	 * @return the mRange
+	 */
+	public int getRange() {
+		return mRange;
+	}
+
+	/**
+	 * @param mRange the mRange to set
+	 */
+	public void setRange(int mRange) {
+		this.mRange = mRange;
+	}
+
+	/**
+	 * @return the mSpecialProperties
+	 */
+	public String getSpecialProperties() {
+		return mSpecialProperties;
+	}
+
+	/**
+	 * @param mSpecialProperties the mSpecialProperties to set
+	 */
+	public void setSpecialProperties(String mSpecialProperties) {
+		this.mSpecialProperties = mSpecialProperties;
+	}
+
+	/**
+	 * @return the mAmmunition
+	 */
+	public int getAmmunition() {
+		return mAmmunition;
+	}
+
+	/**
+	 * @param mAmmunition the mAmmunition to set
+	 */
+	public void setAmmunition(int mAmmunition) {
+		this.mAmmunition = mAmmunition;
+	}
+
+	/**
+	 * @return the mType
+	 */
+	public String getType() {
+		return mType;
+	}
+
+	/**
+	 * @param mType the mType to set
+	 */
+	public void setType(String mType) {
+		this.mType = mType;
+	}
+
+	/**
+	 * @return the mSize
+	 */
+	public String getSize() {
+		return mSize;
+	}
+
+	/**
+	 * @param mSize the mSize to set
+	 */
+	public void setSize(String mSize) {
+		this.mSize = mSize;
+	}
+
+	public int getTypeInt(Context context) {
+		Resources r = context.getResources();
+		String[] options = r.getStringArray(R.array.weapon_type_options);
+		for(int i = 0; i < options.length; i++) {
+			if(mType.equals(options[i]))
+				return i;
+		}
+		return 0;
+	}
+	
+	
+}
---
+++ PTDepLib/src/com/lateensoft/pathfinder/toolkit/deprecated/v1/model/character/stats/PTAbilityScore.java
@@ -0,0 +1,76 @@
+package com.lateensoft.pathfinder.toolkit.deprecated.v1.model.character.stats;
+
+
+public class PTAbilityScore {
+	private String mAbility;
+	private int mScore;
+	
+	public PTAbilityScore(String ability, int score) {
+		mAbility = ability;
+		mScore = score;
+	}
+	
+	public int getScore() {
+		return mScore;
+	}
+	
+	public String getAbility() {
+		return mAbility;
+	}
+	
+	public void setScore(int score) {
+		mScore = score;
+	}
+	
+	public void setAbility(String ability) {
+		mAbility = ability;
+	}
+	
+	public int getModifier() {
+		float temp = (float) ((mScore - 10)/2.0);
+		if (temp < 0) {
+			return (int) (temp - 0.5);
+		} else {
+			return (int) temp;
+		}
+	}
+	
+	public void incScore() {
+		if ((mScore + 1) > 18) {
+			return;
+		} else {
+			mScore++;
+			return;
+		}
+	}
+	
+	public void decScore() {
+		if ((mScore - 1) < 7) {
+			return;
+		} else {
+			mScore--;
+			return;
+		}
+	}
+	
+	// Takes a raw ability score
+	// Returns the point cost
+	public int getAbilityPointCost() {
+		if (mScore <= -7) {
+			return -3;
+		} else if (mScore == 8) {
+			return -2;
+		} else if (mScore == 9) {
+			return -1;
+		} else if (mScore <= 13) {
+			return mScore - 10;
+		} else if (mScore <= 15) {
+			return (2 * (mScore - 14)) + 5;
+		} else if (mScore <= 17) {
+			return (3 * (mScore - 16)) + 10;
+		} else {
+			return 17;
+		}
+		
+	}
+}
